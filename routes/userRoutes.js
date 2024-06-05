b1e0dff864890c479077ede120b0b63d174cc337
oshodiomolola <iamzoey323@gmail.com>
2024-06-05 15:16:37 +0100

    Might be done now

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const jwt = require("../middlewares/authMiddleware")
 const {
   profile,
   updateProfilePhoto,
 } = require("../controllers/userController");
-const { jwToken } = require("../utils/jwt");
+// const { jwToken } = require("../utils/jwt");
 
 const userRouter = express.Router();
 
-userRouter.get("/profile", jwToken, profile);
-userRouter.post("/updatePhoto", jwToken, updateProfilePhoto);
+userRouter.get("/profile", jwt, profile);
+userRouter.post("/updatePhoto", jwt, updateProfilePhoto);
 
 module.exports = { userRouter };
